7c54f8930809bc3eadfcd6dd73c8b1ab3ee55ac2
mika.benoit <mika.benoit@gmail.com>
2022-11-17 12:44:58 +0100

    The car is now red with light... It can drift too

--- Assets/Script/CarController.cs
+++ Assets/Script/CarController.cs
@@ -7,9 +7,15 @@ public class CarController : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
 
+    private bool breaking;
+
+    private float driftTime;
+
     [SerializeField] private float forwardSpeed;
     [SerializeField] private float backwardSpeed;
     [SerializeField] private float turnSpeed;
+    [SerializeField] private float driftPotential;
+    [SerializeField] private float driftForce;
 
     [SerializeField] Rigidbody sphereRigidbody;
 
@@ -24,16 +30,25 @@ public class CarController : MonoBehaviour
 
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+        breaking = Input.GetKey(KeyCode.Space);
 
         verticalInput *= verticalInput > 0 ? forwardSpeed : backwardSpeed;
-
+        if(breaking){
+            verticalInput = 0;
+            horizontalInput *= driftForce * driftTime/driftPotential;
+            driftTime -= Time.deltaTime;
+            driftTime = driftTime < 0 ? 0 : driftTime;
+        } else if(driftTime < driftPotential){
+            driftTime += Time.deltaTime;
+        }
         transform.position = sphereRigidbody.transform.position;
-
-        float newRotation = horizontalInput * turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
-        transform.Rotate(0, newRotation, 0, Space.World);
+        if(driftTime > 0){
+            float newRotation = horizontalInput * turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+            transform.Rotate(0, newRotation, 0, Space.World);
+        }
     }
 
-    private void FixedUpdate(){
+    private void FixedUpdate() {
         sphereRigidbody.AddForce(transform.forward * verticalInput, ForceMode.Acceleration);
     }
 }
