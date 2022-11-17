07ed0c18f9602fd702183ae942104fcf5b4702de
mika.benoit <mika.benoit@gmail.com>
2022-11-17 18:01:02 +0100

    Score, UI and timer

--- Assets/Script/CheckpointSpawner.cs
+++ Assets/Script/CheckpointSpawner.cs
@@ -5,22 +5,52 @@ using UnityEngine;
 public class CheckpointSpawner : MonoBehaviour
 {
     [SerializeField] public GameObject checkpointPrefab;
+    [SerializeField] private ScoreManager scoreManager;
+
+    [SerializeField] private float resetTime;
+
+    private bool started = false;
+
+    private float remainingTime;
+
+    private GameObject lastCheckPoint;
 
     // Start is called before the first frame update
     void Start()
     {
+        remainingTime = resetTime;
         SpawnRandomCheckpoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(started){
+            remainingTime -= Time.deltaTime;
+            remainingTime = remainingTime < 0 ? 0 : remainingTime;
+            scoreManager.UpdateTimer(remainingTime);
+        }
+        if(remainingTime == 0){
+            Stop();
+        }
     }
 
     void SpawnRandomCheckpoint(){
-        GameObject checkpoint = Instantiate(checkpointPrefab, new Vector3(Random.Range(-50, 50) , 0.5F, Random.Range(-50, 50)), Quaternion.identity);
-        CheckPoint script = checkpoint.GetComponent<CheckPoint>();
-        script.setCallbackAction((SpawnRandomCheckpoint));
+        lastCheckPoint = Instantiate(checkpointPrefab, new Vector3(Random.Range(-50, 50) , 0.5F, Random.Range(-50, 50)), Quaternion.identity);
+        CheckPoint script = lastCheckPoint.GetComponent<CheckPoint>();
+        script.setCallbackAction((Next));
+    }
+
+    void Next(){
+        if(!started){
+            started = true;
+        }
+        scoreManager.incrementScore();
+        remainingTime = resetTime;
+        SpawnRandomCheckpoint();
+    }
+
+    void Stop(){
+        Destroy(lastCheckPoint);
     }
 }
---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    [SerializeField] public TMP_Text textScore;
+    [SerializeField] public TMP_Text textTimer;
+    private float score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textScore.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void incrementScore(){
+        score += 1;
+        textScore.text = score.ToString();
+    }
+
+    public void UpdateTimer(float remaining){
+        textTimer.text = remaining.ToString("0.00");
+    }
+
+}
